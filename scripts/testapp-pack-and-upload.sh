72f6650b561064e1ae68893330a6d13e0cfe3f9c
Dominik Sokal <dominik.sokal@swmansion.com>
2020-07-10 11:03:34 +0200

    fix testapp-pack-and-upload.sh

--- scripts/testapp-pack-and-upload.sh
+++ scripts/testapp-pack-and-upload.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
 ARCHIVE_NAME="testapp-$(date +%s).tar.gz"
 ARCHIVE_S3_URI="s3://turtle-v2-test-fixtures/$ARCHIVE_NAME"
 
-git archive --format=tar.gz -o $ARCHIVE_NAME HEAD $SCRIPT_DIR/..
+git archive --format=tar.gz --prefix project/ -o $ARCHIVE_NAME HEAD $SCRIPT_DIR/..
 aws s3 cp $ARCHIVE_NAME $ARCHIVE_S3_URI
 
 rm -rf $ARCHIVE_NAME
